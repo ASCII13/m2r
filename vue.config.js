80e276c1ee637c9ab1d755fdd2755cc086f9986a
wangzhiwen <wangzhiwen@51tiangou.com>
2022-01-06 09:20:57 +0800

    add compression plugin for building

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
     publicPath: './',
     devServer: {
         port: 8888
+    },
+    productionSourceMap: false,
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugins.push(
+                new CompressionPlugin({
+                    algorithm: 'gzip',
+                    test: /\.js$|\.html$|\.css$/,
+                    threshold: 10240,
+                    minRatio: 0.8,
+                    deleteOriginalAssets: false
+                })
+            );
+        }
     }
 }
